9667a0f67a88d93721b4bf1cbb59b2ef7f32b261
Kristofer Rye <kristofer.rye@gmail.com>
2019-02-17 17:37:18 -0600

    cargo: Add Rocket as a dependency and begin using it
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>
    Tested-by: Kristofer Rye <kristofer.rye@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+#[macro_use]
+extern crate rocket;
+extern crate rocket_contrib;
+
+/// Returns a fully-assembled Rocket, ready for ignition.
+fn server() -> rocket::Rocket {
+	rocket::Rocket::ignite()
+}
+
 fn main() {
 	println!("Hello world!");
+
+	server().launch();
 }
