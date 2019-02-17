ba6bdbc330dbd2993b757a0bdcb0705ac151c1fd
Kristofer Rye <kristofer.rye@gmail.com>
2019-02-17 17:38:29 -0600

    server: Add static files routing for /public/
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>
    Tested-by: Kristofer Rye <kristofer.rye@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,16 @@
 extern crate rocket;
 extern crate rocket_contrib;
 
+use std::path::Path;
+
+use rocket_contrib::serve::StaticFiles;
+
 /// Returns a fully-assembled Rocket, ready for ignition.
 fn server() -> rocket::Rocket {
+	let static_dir: &Path = Path::new(concat!(env!("CARGO_MANIFEST_DIR"), "/public"));
+
 	rocket::Rocket::ignite()
+		.mount("/", StaticFiles::from(static_dir))
 }
 
 fn main() {
