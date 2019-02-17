6d7d314c22af571f7bd37d7fe8e2cfbc3598cc51
Kristofer Rye <kristofer.rye@gmail.com>
2019-02-17 11:22:02 -0600

    Remove Ruby code
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>
    Tested-by: Kristofer Rye <kristofer.rye@gmail.com>

--- Gemfile
+++
@@ -1,4 +0,0 @@
-source 'https://rubygems.org'
-
-gem 'rack', '~> 2.0.6'
-gem 'sinatra', '~> 2.0.5'
--- site.rb
+++
@@ -1,5 +0,0 @@
-require 'sinatra'
-
-get '/' do
-	erb :'index.html', locals: {for_hire: true}
-end
