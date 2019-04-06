643e840fb6415a7bdf734daf85c4c8dfe01c5c66
Kristofer Rye <kristofer.rye@gmail.com>
2019-04-06 14:34:00 -0500

    Switch to Jekyll
    [GitHub] This commit resolves #17.
    
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>
    Tested-by: Kristofer Rye <kristofer.rye@gmail.com>

--- src/lib.rs
+++
@@ -1,6 +0,0 @@
-extern crate rocket;
-
-mod server;
-mod telemetry;
-pub use server::*;
-pub use telemetry::*;
--- src/telemetry.rs
+++
@@ -1,6 +0,0 @@
-pub trait ErrorReporter {
-	fn report();
-}
-
-mod sentry_error_reporter_fairing;
-pub use sentry_error_reporter_fairing::*;
